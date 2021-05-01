5885aee0b8bdda72bb8ac100c04d39721d4c9976
inphobia <nick.nauwelaerts@aquafin.be>
2021-05-01 22:57:45 +0200

    lets try with 5.8 since we need index caching

--- lib/Alien/SNMP/MAXTC.pm
+++ lib/Alien/SNMP/MAXTC.pm
@@ -5,7 +5,7 @@ use warnings;
 use 5.010001;
 use parent qw(Alien::Base);
 
-our $VERSION = '1.002000';
+our $VERSION = '1.003000';
 
 1;
 
@@ -21,7 +21,7 @@ Alien::SNMP::MAXTC - Alien package for the Net-SNMP library
 
 =head1 VERSION
 
-Version 1.000001
+Version 1.003000
 
 =cut
 
